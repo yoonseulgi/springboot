25d7ffd95c4b3c79bd45dc75b258c8ff4337d094
yoonseulgi <tmffml1232@gmail.com>
2021-10-18 20:34:03 +0900

    프로젝트 생성

---
+++ springboot/src/test/java/com/koscom/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koscom.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest  //springboot 환경의 서비스가 가동될 수 있도록 기본 환경 설정을 자동으로 해줌.
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
